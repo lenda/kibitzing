e246767ea5726a3dcc275cff66b42840ef19490e
Eleanor Howard <ellie@lizthedeveloper.com>
2017-04-19 21:42:00 -0700

    fixed pdf js viewer bug. added cors middleware to server.

--- app.js
+++ app.js
@@ -3,6 +3,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 app.use('/public', express.static('public'));
 
--- main.js
+++ main.js
@@ -1,78 +1,27 @@
-const PDFJS = require('pdfjs-dist');
-var pdfWorker = require('pdfjs-dist/build/pdf.worker')
+window.onload = function(){
+var url = 'http://s3-us-west-1.amazonaws.com/pdf-dev-learning/2013hmda-guide.pdf';
 
-var url = '//cdn.mozilla.net/pdfjs/helloworld.pdf';
-
-// // Disable workers to avoid yet another cross-origin issue (workers need
-// // the URL of the script to be loaded, and dynamically loading a cross-origin
-// // script does not work).
-// // PDFJS.disableWorker = true;
-//
-// // The workerSrc property shall be specified.
-//
-// // Asynchronous download of PDF
-// var loadingTask = PDFJS.getDocument(url);
-// loadingTask.promise.then(function(pdf) {
-//   console.log('PDF loaded');
-//
-//   // Fetch the first page
-//   var pageNumber = 1;
-//   pdf.getPage(pageNumber).then(function(page) {
-//     console.log('Page loaded');
-//
-//     var scale = 1.5;
-//     var viewport = page.getViewport(scale);
-//
-//     // Prepare canvas using PDF page dimensions
-//     var canvas = document.getElementById('the-canvas');
-//     var context = canvas.getContext('2d');
-//     canvas.height = viewport.height;
-//     canvas.width = viewport.width;
-//
-//     // Render PDF page into canvas context
-//     var renderContext = {
-//       canvasContext: context,
-//       viewport: viewport
-//     };
-//     var renderTask = page.render(renderContext);
-//     renderTask.then(function () {
-//       console.log('Page rendered');
-//     });
-//   });
-// }, function (reason) {
-//   // PDF loading error
-//   console.error(reason);
-// });
-var fs = require('fs');
-var data = new Uint8Array(fs.readFileSync('2013hmda-guide.pdf'));
-// var worker = require('webworkerify')("./node_modules/pdfjs-dist/build/pdf.worker");
-//
 PDFJS.disableWorker = true;
-PDFJS.workerSrc = "./node_modules/pdfjs-dist/build/pdf.worker";
+// The workerSrc property shall be specified.
+PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+PDFJS.getDocument(url).then(function(pdf) {
+  // you can now use *pdf* here
+pdf.getPage(1).then(function(page) {
+    var scale = 1.5;
+    var viewport = page.getViewport(scale);
 
-PDFJS.getDocument(data).then(function(pdf) {
-    console.log("#1");
-    pdf.getPage(1).then(function(page) {
-        console.log('Number of pages: ' + pdf.numPages);
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
 
-        var scale = 1.5;
-        var viewport = page.getViewport(scale);
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    page.render(renderContext);
 
-        var canvas = document.getElementById('the-canvas');
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        var renderContext = {
-          canvasContext: context,
-          viewport: viewport
-        };
-        console.log("just before page render");
-        page.render(renderContext);
-    });
+});
 });
 
-
-
-
-console.log("working");
+};
